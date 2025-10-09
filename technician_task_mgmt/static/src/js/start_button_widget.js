5fd09f4371acb080cb6d621c69a4714aca275617
BarredChip45 <101852998+BarredChip45@users.noreply.github.com>
2025-10-09 15:31:13 +0200

    Add files via upload

---
+++ technician_task_mgmt/static/src/js/start_button_widget.js
@@ -0,0 +1,7 @@
+/** @odoo-module **/
+
+// Empty module to prevent loading errors
+// This file exists only to prevent module loading errors
+// The actual functionality is in task_start_widget.js
+
+console.log('start_button_widget.js loaded (empty placeholder)');
